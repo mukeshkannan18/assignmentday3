6ef3e5a978e7a3a6c643d8b51a9108c50d691a33
mukeshkannan18 <115523174+mukeshkannan18@users.noreply.github.com>
2022-10-13 10:35:53 +0530

    Add files via upload

---
+++ PositiveandNegativenumbers.java
@@ -0,0 +1,15 @@
+package week1day3;
+
+public class PositiveandNegativenumbers {
+	public static void main(String[] args) {
+		int number=99;
+		if(number>0) {
+			System.out.println("the number is positive");
+			
+		}
+		else if(0>number){
+			System.out.println("the number is negative");
+		}
+	}
+
+}
